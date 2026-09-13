a2642319f0133e9d0535be8a900a1164c639ae01
Razibul Islam <razibul017@gmail.com>
2026-09-13 22:19:16 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline caching & auto update
 ========================================================= */
 
-const CACHE_NAME = 'campus-calendar-v1.0.0';
+const CACHE_NAME = 'campus-calendar-v1.0.5';
 
 
 const PRECACHE_URLS = [
